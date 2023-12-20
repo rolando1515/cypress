5341aefcd690d406dffbd2ad4679c9ac116fe012
Roland Labrador <rolando1515@hotmail.com>
2023-12-20 16:16:14 -0500

    commit

--- cypress/e2e/forms.cy.js
+++ cypress/e2e/forms.cy.js
@@ -6,12 +6,18 @@ describe('Form tests', () => {
       })
       it ('Test subscription form', () => {
         cy.contains(/testing forms/i)
-        cy.getDataTest ('subscribe-form').find('input').type('roland@gmail.com')
+        cy.getDataTest ('subscribe-form').find('input').as('subscribe-input')
+        cy.get('@subscribe-input').type('roland@gmail.com')
         cy.contains(/Successfully subbed:roland@gmail.com!/i).should('not.exist')    
         cy.getDataTest ('subscribe-button').click() 
         cy.contains(/Successfully subbed: roland@gmail.com!/i).should('exist')
         cy.wait(3000)
+        cy.contains(/Successfully subbed: roland@gmail.com!/i).should('not.exist')
 
+
+        cy.get('@subscribe-input').type('roland@gmail.io')
+        cy.getDataTest ('subscribe-button').click()   
+        cy.contains(/Invalid email: roland@gmail.io!/i).should('exist')
 
       })  
 
